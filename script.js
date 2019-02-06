46f2e7fefb37ef7d5c49955f73c35b738df52087
Andra <andra.popa1989@gmail.com>
2019-02-06 16:19:58 +0000

    add header

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class Jobs extends React.Component {
     }
     
     componentDidMount() {
-        fetch('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=1&search=london') 
+        fetch('https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?description=python') 
             .then(response => response.json())
             .then(data => {
                 this.setState({
@@ -21,14 +21,17 @@ class Jobs extends React.Component {
         const jobs = this.state.jobs.map((job, index) => {
             let d = this.state.jobs[index].created_at;
                 d = d.split(' ');
-                let my_date = d[1] +' ' + d[2] + ' ' + d[5];
+                let my_date = d[2] +' ' + d[1] + ' ' + d[5];
             return (
                 <div className="card" key={index}>
                     <h1>{this.state.jobs[index].title}</h1>
                     <p>{this.state.jobs[index].type}</p>
-                    <p>{this.state.jobs[index].location}</p>
+                    <p>Location: {this.state.jobs[index].location}</p>
                     <p>Company: {this.state.jobs[index].company}</p>
-                    <p>{ my_date }</p>
+                    <p>Date: { my_date }</p>
+                    <a href={this.state.jobs[index].url} target='_blank'>
+                        Read more
+                    </a>
                 </div>
             )
         
@@ -37,6 +40,7 @@ class Jobs extends React.Component {
             <div className='container'>
                 <header>
                     <h1>GitHub Jobs</h1>
+                    <p>Researching Python developers</p>
                 </header>
                 <main className="cards-container">
                     { jobs }
